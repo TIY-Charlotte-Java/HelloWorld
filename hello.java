4752b50fc36a1be91156fdbcb9ce89813aa7359b
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-11-28 11:55:19 -0500

    converting from english to java

--- hello.java
+++ hello.java
@@ -1,1 +1,8 @@
-Hell World
+class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    }
+}
+
+
+// basic unit of java code is a class. at the very minimum classes contain methods. Methods are things that do stuff. -- there is a special function in java, called Main. Also a special method? called main. It tells the code where to start. --- println is the name of the method...the argument is the string. println is a method that accepts one argument. Double quotes for all strings. 
